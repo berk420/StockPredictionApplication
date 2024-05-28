5ebec8122fdb180de875939f7ee2d30d5abea1bf
berk gümüş <63772343+berk420@users.noreply.github.com>
2024-05-28 18:28:05 +0300

    added front file

--- StockAPI/StockAPI/Services/IStockDbService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StockAPI.Services
-{
-    public interface IStockDbService
-    {
-        Task<T> GetAsync<T>(string command, object parms);
-        Task<List<T>> GetAll<T>(string command, object parms);
-    }
-}
